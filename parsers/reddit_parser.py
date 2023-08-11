8d54f9ff39a8528b540db311628a81f13b44a4f2
filippzotov <filippzotov1998@gmail.com>
2023-08-11 19:55:39 +0200

    added csv writer

--- parsers/reddit_parser.py
+++ parsers/reddit_parser.py
@@ -1,28 +1,84 @@
 import requests
 from bs4 import BeautifulSoup
-
-url = "https://www.reddit.com/svc/shreddit/community-more-posts/top/?t=DAY&name=Basketball&after=dDNfMTVuZ241dw=="
-# url = "https://old.reddit.com/r/Basketball/top/?sort=top&t=week&after=dDNfMTVuajl4aQ=="dDNfMTVuZ241dw
-
-headers = {
-    "Accept": "*/*",
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/115.0",
-}
-
-req = requests.get(url, headers=headers, timeout=5)
-
-src = req.text
-# print(src)
-with open("index.html", "w", encoding="utf-8") as file:
-    file.write(src)
+import csv
 
 
-with open("index.html", encoding="utf-8") as file:
-    src = file.read()
+def get_posts_from_page(url, headers):
+    req = requests.get(url, headers=headers, timeout=5)
+    src = req.text
+    soup = BeautifulSoup(src, "lxml")
+    next_page_links = soup.find_all(
+        class_="block cursor-pointer relative bg-neutral-background focus-within:bg-neutral-background-hover hover:bg-neutral-background-hover xs:rounded-[16px] p-md my-2xs nd:visible"
+    )
+    next_page = None
+    for link in next_page_links:
+        if link.get("more-posts-cursor") is not None:
+            next_page = link.get("more-posts-cursor")
+    return (next_page_links, next_page)
 
-soup = BeautifulSoup(src, "lxml")
-all_posts = soup.find_all(class_="absolute inset-0")
-# print(soup.find())
-print(len(all_posts))
-for post in all_posts:
-    print(post)
+
+def get_reddit_posts(duration, subreddit):
+    base_url = f"https://www.reddit.com/svc/shreddit/community-more-posts/top/?t={duration}&name={subreddit}"
+    headers = {
+        "Accept": "*/*",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/115.0",
+    }
+    all_posts = []
+    url = base_url
+    while True:
+        posts = get_posts_from_page(url, headers)
+        all_posts.extend(posts[0])
+        if posts[1] is None or len(posts[0]) < 25:
+            break
+        url = base_url + f"&after={posts[1]}=="
+    return all_posts
+
+
+def get_data_from_html(posts):
+    answer = []
+    for post in posts:
+        url = post["content-href"]
+        title = post["post-title"]
+        author = post["author"]
+        rating = post.get("score")
+        date = post["created-timestamp"]
+
+        text = post.find(
+            class_="text-neutral-content md max-h-[337px] overflow-hidden text-12 xs:text-14"
+        )
+        if text:
+            text = text.text
+        tmp = {
+            "author": author,
+            "title": title,
+            "text": text,
+            "rating": rating,
+            "date": date,
+            "url": url,
+        }
+        answer.append(tmp)
+    return answer
+
+
+def write_to_csv(periud, subreddit, posts):
+    fieldnames = ["author", "title", "text", "rating", "date", "url"]
+    with open(
+        f"{subreddit}_{periud}.csv", "w", encoding="utf-8", newline=""
+    ) as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(posts)
+
+
+if __name__ == "__main__":
+    periud = ["HOUR", "DAY", "WEEK", "MONTH"]
+    print("Enter subreddit name")
+    subreddit = input()
+    p_index = 0
+    while p_index < 1 or p_index > 4:
+        print("Chose periud of time:\n1. hour\n2. day\n3. week\n4. month")
+        p_index = int(input())
+    html_posts = get_reddit_posts(periud[p_index - 1], subreddit)
+    posts = get_data_from_html(html_posts)
+    write_to_csv(periud[p_index - 1], subreddit, posts)
+    print("Done")
